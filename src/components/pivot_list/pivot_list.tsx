b76f302ea3d6494a8e504a1ca621c2e9c8084a7f
PNowakowski <p.nowakowskimusic@gmail.com>
2019-12-11 23:11:18 -0800

    Create css reset and stylesheet for pivot_list component

--- src/components/pivot_list/pivot_list.tsx
+++ src/components/pivot_list/pivot_list.tsx
@@ -17,7 +17,7 @@ class PivotList extends React.Component<Props, State>  {
     
     render(): ReactElement {
         return (
-            <div>
+            <div className="pivot-list">
                 <header className="header">
                     <div>
                         <button>Gridify</button>
@@ -43,55 +43,57 @@ class PivotList extends React.Component<Props, State>  {
                         </span>
                     </div>
                 </header>
-                <aside className="left-drawer">
-                    <div>
-                        <span>
-                            <label htmlFor="sort-by">Sort By</label>
-                            <select name="sort-by" id="sort-by">
-                                <option value="day">Day</option>
-                                <option value="week">Week</option>
-                                <option value="month">Month</option>
-                                <option value="year">Year</option>
-                            </select>
-                        </span>
-                    </div>
-                    <div>
-                        <span>
-                            <h4>Date Range</h4>
-                        </span>
-                        <span>
-                            <h4>Date Range</h4>
-                        </span>
-                    </div>
-                    <button>Left icon</button>
-                </aside>
-                <section className="photo-grid">
-                    <span>
-                        <figure>
-                            <img src="" alt="Data Image"/>
-                            <figcaption>image</figcaption>
-                        </figure>
-                    </span>
-                    <h3>Image Size</h3>
-                    <button>- icon</button>
-                    <button>+ icon</button>
-                </section>
-                <div className="right-drawer">
-                    <details>
-                        <summary>Data</summary>
+                <main>
+                    <aside className="left-drawer open">
                         <div>
                             <span>
-                                <label htmlFor="label">Label:</label>
-                                <input type="text" value="Label Name"/>
-                            </span>
-                            <span>
-                                <label htmlFor="label">Label:</label>
-                                <input type="text" value="Label Name" />
+                                <label htmlFor="sort-by">Sort By</label>
+                                <select name="sort-by" id="sort-by">
+                                    <option value="day">Day</option>
+                                    <option value="week">Week</option>
+                                    <option value="month">Month</option>
+                                    <option value="year">Year</option>
+                                </select>
                             </span>
                         </div>
-                    </details>
-                    <button>Right icon</button>
-                </div>
+                        <div>
+                            <span>
+                                <h4>Date Range</h4>
+                            </span>
+                            <span>
+                                <h4>Date Range</h4>
+                            </span>
+                        </div>
+                        <button>Left icon</button>
+                    </aside>
+                    <section className="photo-grid">
+                        <span>
+                            <figure>
+                                <img src="" alt="Data Image"/>
+                                <figcaption>image</figcaption>
+                            </figure>
+                        </span>
+                        <h3>Image Size</h3>
+                        <button>- icon</button>
+                        <button>+ icon</button>
+                    </section>
+                    <div className="right-drawer">
+                        <details>
+                            <summary>Data</summary>
+                            <div>
+                                <span>
+                                    <label htmlFor="label">Label:</label>
+                                    <input type="text" value="Label Name"/>
+                                </span>
+                                <span>
+                                    <label htmlFor="label">Label:</label>
+                                    <input type="text" value="Label Name" />
+                                </span>
+                            </div>
+                        </details>
+                        <button>Right icon</button>
+                    </div>
+                </main>
             </div>
         )
     }
--- src/root.tsx
+++ src/root.tsx
@@ -3,6 +3,7 @@ import { HashRouter } from 'react-router-dom';
 
 import App from './App';
 
+
 const Root = () => (
     <HashRouter>
         <App />
