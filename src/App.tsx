6cf161cd2b13772adc791bea16c0d2f84110ee01
PNowakowski <p.nowakowskimusic@gmail.com>
2019-12-11 20:44:43 -0800

    Setup PivotList component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Switch, Route, Redirect, Link } from 'react-router-dom';
 import './App.css';
 import List from './components/list';
 import Header from './header';
+import PivotList from './components/pivot_list';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/list" component={Header}/>
+        <Route exact path="/pivot-list" component={PivotList}/>
         <Route path="/" component={List}/>
       </Switch>
     </div>
---
+++ src/components/pivot_list.tsx
@@ -0,0 +1,26 @@
+import React, { ReactElement } from 'react';
+
+interface Props {
+
+}
+
+interface State {
+
+}
+
+class PivotList extends React.Component<Props, State>  {
+    constructor(props: Props) {
+        super(props);
+        this.state = {};
+    }
+    
+    render(): ReactElement {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default PivotList;
